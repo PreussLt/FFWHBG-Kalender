67b9512b188130e8125fd952f2e5fb3359d29f8d
Kyle Henselmann <44337037+PreussLt@users.noreply.github.com>
2026-03-21 16:22:42 +0100

    Add JUF an IUK to abt. 1

--- abt_1.php
+++ abt_1.php
@@ -21,8 +21,10 @@ $data = getDataFromJson($filename, $calurl);
 // Arrays aus der Data holen,
 getArrays($data, $gruppe1, "XYZ", $gruppe2, "XYZ", $gruppe3, "XYZ", $gruppe4, "XYZ", $hbg);
 
-//Weiteren Kalender zur URL hinzufügen
+// Weiteren Kalender zur URL hinzufügen
 addCalender("./ical/ffw.json", "https://calsync.alamos-gmbh.com/calendar/ical/technik%40leitstelle-boeblingen.de/public-BJzrvdoQS8ospFjntN9UZ300pscvGBrW/cal.ics", $gesamt);
+addCalender("./ical/iuk.json", "https://calsync.alamos-gmbh.com/calendar/ical/technik%40leitstelle-boeblingen.de/public-OhSMU2WZrTysDKnu9PZgodKvxphIYUsQ/cal.ics", $gesamt);
+addCalender("./ical/juf.json", "https://calsync.alamos-gmbh.com/calendar/ical/technik%40leitstelle-boeblingen.de/public-j3iccbU8NXO7609IzZCNgHEJVy5z4WHI/cal.ics", $gesamt);
 
 // Ausgabe der Dateien in eine Gruppe:
 showTable($hbg, "Abt. Herrenerg", $gruppe2, "0", $gruppe3, "0", $gruppe3, "0", $gesamt, "Gesamt");
