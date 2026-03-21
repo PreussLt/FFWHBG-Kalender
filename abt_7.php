3fab1ca17c6a1f8af7ff4186f729b6b34a6d1df8
Kyle Henselmann <44337037+PreussLt@users.noreply.github.com>
2026-03-21 18:55:02 +0100

    Aktualisieren von abt_7.php

--- abt_7.php
+++ abt_7.php
@@ -22,6 +22,7 @@ getArrays($data,$gruppe1,"Gr. 1 -",$gruppe2,"Gr. 2 -",$gruppe3,"Gr. 3 -",$gruppe
 //Weiteren Kalender zur URL hinzufügen
 addCalender("./ical/ffw.json","https://calsync.alamos-gmbh.com/calendar/ical/technik%40leitstelle-boeblingen.de/public-BJzrvdoQS8ospFjntN9UZ300pscvGBrW/cal.ics",$gesamt);
 addCalender("./ical/laz.json","https://calsync.alamos-gmbh.com/calendar/ical/technik%40leitstelle-boeblingen.de/public-kk8zgJUYegpzDfwTmS57MI0EycIiMy0m/cal.ics",$gesamt);
+addCalender("./ical/ffw.json","https://calsync.alamos-gmbh.com/calendar/ical/technik%40leitstelle-boeblingen.de/public-v6ejoET3mLE2ofKIvKJ1QYTDLKL04EUe/cal.ics",$gesamt);
 // Ausgabe der Dateien in eine Gruppe:
 showTable($gruppe1,"Gruppe 1",$gruppe2,"Gruppe 2",$gruppe3,"Gruppe 3",$gruppe3,"0",$gesamt,"Gesamt");
 ?>
